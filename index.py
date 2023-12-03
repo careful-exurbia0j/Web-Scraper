c5aca1c2f5346b741a6fc45c30631bedc3f5dd62
Mia <152382476+careful-exurbia0j@users.noreply.github.com>
2023-12-04 03:24:12 +0700

    Create index.py

---
+++ index.py
@@ -0,0 +1,8 @@
+# Python Web Scraper BeautifulSoup
+from bs4 import BeautifulSoup
+import requests
+
+url = 'https://example.com'
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'html.parser')
+print(soup.title.text)
